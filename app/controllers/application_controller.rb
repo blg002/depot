b993571cf7588541368a98aa84135e21d8f06338
Brad Graham <blg002@gmail.com>
2012-06-26 21:28:54 -0700

    chapter 8 extras

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery
+
+  helper_method :date_loaded
+  before_filter :set_date
+
+  def date_loaded 
+    return Time.now.strftime('%I:%M %p')
+  end
+
+  def set_date
+    @date_loaded = Time.now.strftime('%I:%M %p')
+  end
+
 end

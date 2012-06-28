ff3baa8ce45ff32b43ab94ced6ccd6557af84057
Brad Graham <blg002@gmail.com>
2012-06-27 21:34:42 -0700

    finish chapter 9 "Cart Creation"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < ActionController::Base
     @date_loaded = Time.now.strftime('%I:%M %p')
   end
 
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end

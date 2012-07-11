da228ac3f7d77c8a42789e047c81927caf8c6d66
Brad Graham <blg002@gmail.com>
2012-07-10 21:18:44 -0700

    finish chapter 15 "Internationalization"

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,12 +3,16 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
 
-    @session_count = increment_session_count
-    @session_count_message = "You have visited this page #{@session_count} times since your last purchase" if @session_count > 5
+      @session_count = increment_session_count
+      @session_count_message = "You have visited this page #{@session_count} times since your last purchase" if @session_count > 5
 
-    @cart = current_cart
+      @cart = current_cart
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,18 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-
-  resources :orders
-
-  resources :line_items do
-    post 'decrement', on: :member
-  end
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-    get :who_bought, on: :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items do
+      post 'decrement', on: :member
+    end
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
+    get "store/index"
   end
 
   # The priority is based upon order of creation:
@@ -74,7 +72,6 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root to: 'store#index', as: 'store'
 
   # See how all your routes lay out with "rake routes"
 
